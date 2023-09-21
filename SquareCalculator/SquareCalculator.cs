2ae1acc2c1258a39a6c03d3c7497bfae0304faef
Daria <daria_alles@mail.ru>
2023-09-21 19:31:34 +0300

    Add project files.

---
+++ SquareCalculator/SquareCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SquareCalculator
+{
+    public class SquareCalculator
+    {
+
+        public interface IShape
+        {
+            double CalculateSquare();
+        }
+
+        public class Circle : IShape
+        {
+            private double radius;
+
+            public Circle(double radius)
+            {
+                this.radius = radius;
+            }
+
+            public double CalculateSquare()
+            {
+                return Math.PI * radius * radius;
+
+            }
+        }
+
+        public class Triangle : IShape
+        {
+            private double side1, side2, side3;
+
+            public Triangle(double side1, double side2, double side3)
+            {
+                this.side1 = side1;
+                this.side2 = side2;
+                this.side3 = side3;
+            }
+
+            public double CalculateSquare()
+            {
+                double s = (side1 + side2 + side3) / 2;
+                return Math.Sqrt(s * (s - side1) * (s - side2) * (s - side3));
+
+
+            }
+
+            public bool IsRightTriangle()
+            {
+                double[] sides = { side1, side2, side3 };
+                Array.Sort(sides);
+                return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+
+            }
+        }
+    }
+}
---
+++ SquareCalculatorUnitTests/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using static SquareCalculator.SquareCalculator;
+
+namespace SquareCalculatorUnitTests
+{
+    public class SquareCalculatorTests
+    {
+        public static void Main()
+        {
+            TestCalculateCircleArea();
+            TestCalculateTriangleArea();
+
+            TestCalculateRuntimeArea();
+
+            TestIsRightTriangle();
+        }
+
+        public static void TestCalculateCircleArea()
+        {
+            double radius = 5;
+            Circle circle = new Circle(radius);
+            double expectedArea = Math.PI * radius * radius;
+            double actualArea = circle.CalculateSquare();
+
+            Console.WriteLine($"ТЕСТ Площадь круга:");
+            Console.WriteLine($"ОЖИДАНИЕ: {expectedArea:F2}");
+            Console.WriteLine($"ПОЛУЧЕНО: {actualArea:F2}");
+
+            if (expectedArea.Equals(actualArea))
+            {
+                Console.WriteLine("УСПЕШНО");
+            }
+            else
+            {
+                Console.WriteLine("ОШИБКА");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void TestCalculateTriangleArea()
+        {
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+            Triangle triangle = new Triangle(side1, side2, side3);
+            double expectedArea = 6;
+            double actualArea = triangle.CalculateSquare();
+
+            Console.WriteLine($"ТЕСТ Площадь треугольника:");
+            Console.WriteLine($"ОЖИДАНИЕ: {expectedArea:F2}");
+            Console.WriteLine($"ПОЛУЧЕНО: {actualArea:F2}");
+
+            if (expectedArea.Equals(actualArea))
+            {
+                Console.WriteLine("УСПЕШНО");
+            }
+            else
+            {
+                Console.WriteLine("ОШИБКА");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void TestCalculateRuntimeArea() 
+        {
+            IShape shape = new Circle(5.0); 
+
+            double area = shape.CalculateSquare();
+
+            Console.WriteLine($"ТЕСТ Площадь фигуры, реализующей IShape:");
+            Console.WriteLine($"ОЖИДАНИЕ: {area:F2}");
+
+            double expectedArea = Math.PI * 5.0 * 5.0;
+
+            Console.WriteLine($"ПОЛУЧЕНО: {expectedArea:F2}");
+
+            if (Math.Abs(expectedArea - area) < 0.01)
+            {
+                Console.WriteLine("УСПЕШНО");
+            }
+            else
+            {
+                Console.WriteLine("ОШИБКА");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        public static void TestIsRightTriangle()
+        {
+            double side1 = 3;
+            double side2 = 4;
+            double side3 = 5;
+            Triangle triangle = new Triangle(side1, side2, side3);
+            bool isRightTriangle = triangle.IsRightTriangle();
+
+            Console.WriteLine($"ТЕСТ Проверка на прямоугольный треугольник:");
+            Console.WriteLine($"ОЖИДАНИЕ: True");
+            Console.WriteLine($"ПОЛУЧЕНО: {isRightTriangle}");
+
+            if (isRightTriangle)
+            {
+                Console.WriteLine("УСПЕШНО");
+            }
+            else
+            {
+                Console.WriteLine("ОШИБКА");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
